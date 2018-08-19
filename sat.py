82ac3d3827750eabc066728b98eb740142770a6e
Felipe Mogollon <felipemogollonr@gmail.com>
2018-08-19 08:37:34 +0000

    prepare to launch script

--- sat.py
+++ sat.py
@@ -29,13 +29,38 @@ with open("nasa.all") as myFile:
                 tle2=line
 
 
+#Satellite frequencies list from https://www.amsat.org/fm-satellite-frequency-summary/:
+#Los ssb para la siguiente tanda
+freq="0"
+if sat == "AO-85":
+        freq="145.980"
+elif sat == "AO-91":
+        freq="145.960"
+elif sat == "AO-92":
+        freq="145.880"
+elif sat == "SO-50":
+        freq="436.795"                
+elif sat == "LILACSAT-2":
+        freq="437.200 "
+elif sat == "IO-86":
+        freq="435.880"
+elif sat == "ISS":
+        freq="145.800"
+
+
 #Two elements data from any satellite or object
 tle = "%s\n%s%s" %(sat,tle1,tle2)
 #Latitude longitude in degress
 qth = (lat, longitude, alt)  # lat (N), long (W), alt (meters)
 p = predict.transits(tle, qth)
-for i in range(1,10):
+for i in range(1,2):
 	transit = p.next()
         ts=int(transit.start)
-        print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-	print("%f\t%f\t%f" % (transit.start, transit.duration(), transit.peak()['elevation']))
+        #print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+        #print(datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+        day=datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+        hour=datetime.fromtimestamp(ts).strftime('%H:%M')
+        commandline="echo './recordfm.sh %s' | at %s %s" %(freq,day,hour)
+        print commandline
+        #print(datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+	#print("%f\t%f\t%f" % (transit.start, transit.duration(), transit.peak()['elevation']))
